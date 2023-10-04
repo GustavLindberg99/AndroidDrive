d04cb5328c21c7b3a77e663858f67d069b9357d6
GustavLindberg99 <95423695+GustavLindberg99@users.noreply.github.com>
2023-10-04 19:22:06 +0200

    Add files via upload

--- sources/translations/androiddrive_fr.ts
+++ sources/translations/androiddrive_fr.ts
@@ -116,144 +116,149 @@
         <translation>Erreur fatale: Impossible d&apos;énumérer les appareils Android: %1</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="59"/>
+        <location filename="main.cpp" line="61"/>
         <source>&amp;Devices</source>
         <translation>&amp;Appareils</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="62"/>
+        <location filename="main.cpp" line="64"/>
         <source>&amp;Settings</source>
         <translation>&amp;Paramètres</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="65"/>
+        <location filename="main.cpp" line="67"/>
         <source>&amp;About AndroidDrive</source>
         <translation>À propos d&apos;&amp;AndroidDrive</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="70"/>
+        <location filename="main.cpp" line="72"/>
         <source>About AndroidDrive</source>
         <translation>À propos d&apos;AndroidDrive</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="71"/>
+        <location filename="main.cpp" line="73"/>
         <source>AndroidDrive version %1 by Gustav Lindberg.</source>
         <translation>AndroidDrive version %1 par Gustav Lindberg.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="71"/>
+        <location filename="main.cpp" line="73"/>
         <source>Icons made by %3 and %4 from %1 are licensed by %2.</source>
         <translation>Les icônes faites par %3 et %4 pour %1 sont sous license de %2.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="71"/>
+        <location filename="main.cpp" line="73"/>
         <source>This program uses %1 and %2.</source>
         <translation>Ce programme utilise %1 et %2.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="75"/>
+        <location filename="main.cpp" line="77"/>
         <source>About &amp;Qt</source>
         <translation>À propos de Q&amp;t</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="80"/>
+        <location filename="main.cpp" line="82"/>
         <source>E&amp;xit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="8"/>
+        <location filename="settingswindow.cpp" line="10"/>
         <source>Global settings</source>
         <translation>Paramètres globaux</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="9"/>
+        <location filename="settingswindow.cpp" line="11"/>
         <source>Automatially connect &amp;drive</source>
         <translation>Connecter automatiquement le &amp;lecteur</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="10"/>
+        <location filename="settingswindow.cpp" line="12"/>
         <source>Open newly connected drives in &amp;Explorer</source>
         <translation>Ouvrir les lecteurs nouvellement connectés dans l&apos;&amp;Explorateur Windows</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="11"/>
+        <location filename="settingswindow.cpp" line="13"/>
         <source>&amp;Hide files beginning with a dot</source>
         <translation>&amp;Masquer les fichiers commençant par un point</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="12"/>
+        <location filename="settingswindow.cpp" line="14"/>
         <source>&amp;OK</source>
         <translation>&amp;OK</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="13"/>
+        <location filename="settingswindow.cpp" line="15"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Annuler</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="14"/>
+        <location filename="settingswindow.cpp" line="16"/>
         <source>&amp;Apply</source>
         <translation>A&amp;ppliquer</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="18"/>
+        <location filename="settingswindow.cpp" line="20"/>
         <source>AndroidDrive - Settings</source>
         <translation>AndroidDrive - Paramètres</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="24"/>
+        <location filename="settingswindow.cpp" line="26"/>
         <source>Allows you to select a preferred drive letter for the selected Android device. If the preferred drive letter is unavailable when this device is connected, it will use the next available drive letter in alphabetical order. If you change the drive letter while a drive for this device is connected, you will have to disconnect it and re-connect it again for the changes to take effect.</source>
         <translation>Vous permet de sélectionner une lettre de lecteur préférée pour l&apos;appareil Android sélectionné. Si la lettre préférée n&apos;est pas disponible quand le lecteur est connecté, il utilisera la prochaine lettre dans l&apos;ordre alphabétique. Si vous changez la lettre de lecteur lorsque le lecteur est connecté, vous devrez le déconnecter et le reconnecter pour que les changements prennent effet.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="25"/>
+        <location filename="settingswindow.cpp" line="27"/>
         <source>Drive &amp;letter</source>
         <translation>&amp;Lettre de lecteur</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="28"/>
+        <location filename="settingswindow.cpp" line="30"/>
         <source>If this checkbox is checked, the selected device will be automatically connected as a drive whenever you plug it into your computer. Otherwise, you will have to connect it manually by going into Devices &gt; Connect drive.</source>
         <translation>Si cette case est cochée, l&apos;appareil sélectionné sera automatiquement connecté comme lecteur quant vous le branchez à votre ordinateur. Sinon, vous devrez le connecter manuellement dans Appareils &gt; Connecter le lecteur.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="31"/>
+        <location filename="settingswindow.cpp" line="33"/>
         <source>Device settings for %1</source>
         <translation>Paramètres de l&apos;appareil %1</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="32"/>
+        <location filename="settingswindow.cpp" line="34"/>
         <source>These settings only affect the selected device.</source>
         <translation>Ces paramètres ne concernent que l&apos;appareil sélectionné.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="38"/>
+        <location filename="settingswindow.cpp" line="40"/>
         <source>If this checkbox is checked, whenever AndroidDrive is finished connecting a drive, it will open that drive in Windows Explorer.</source>
         <translation>Si cette case est cochée, quand AndroidDrive a fini de connecter un lecteur, il l&apos;ouvrira automatiquement dans l&apos;Explorateur Windows.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="42"/>
+        <location filename="settingswindow.cpp" line="44"/>
         <source>If this checkbox is checked, files that begin with a dot will be treated as hidden files, and will only be visible in Windows Explorer if Windows Explorer&apos;s &quot;Show hidden files&quot; option is activated.</source>
         <translation>Si cette case est cochée, les fichiers commençant avec un point seront considérés comme des fichiers cachés, et seront uniquement visible dans l&apos;Explorateur Windows si le paramètre &quot;Afficher les fichiers cachés&quot; est activé dans l&apos;Explorateur Windows.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="46"/>
+        <location filename="settingswindow.cpp" line="51"/>
         <source>Use system language</source>
         <oldsource>Same as system language</oldsource>
         <translation>Utiliser la langue du système</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="51"/>
-        <source>Allows you to select which language AndroidDrive&apos;s GUI will be displayed in. If you select &quot;Use system language&quot; but AndroidDrive isn&apos;t availiable in your system language, English will be used. This setting has no effect on how the actual drive works.</source>
-        <oldsource>Allows you to select which language AndroidDrive&apos;s GUI will be displayed in. If you select &quot;Same as system language&quot; but AndroidDrive isn&apos;t availiable in your system language, English will be used. This setting has no effect on how the actual drive works.</oldsource>
-        <translation>Vous permet de sélectionner la langue de l&apos;interface graphique d&apos;AndroidDrive. Si vous sélectionnez &quot;Utiliser la langue du système&quot; mais AndroidDrive n&apos;est pas disponible dans la langue de votre système, l&apos;anglais sera utilisée. Ce paramètre n&apos;a aucun effet sur le fonctionnement du lecteur en soi.</translation>
+        <location filename="settingswindow.cpp" line="57"/>
+        <source>Allows you to select which language AndroidDrive&apos;s GUI will be displayed in. If you select &quot;Use system language&quot; but AndroidDrive isn&apos;t availiable in your system language, English will be used. This setting has no effect on how the actual drive works. You must restart AndroidDrive for this change to take effect.</source>
+        <oldsource>Allows you to select which language AndroidDrive&apos;s GUI will be displayed in. If you select &quot;Use system language&quot; but AndroidDrive isn&apos;t availiable in your system language, English will be used. This setting has no effect on how the actual drive works.</oldsource>
+        <translation>Vous permet de sélectionner la langue de l&apos;interface graphique d&apos;AndroidDrive. Si vous sélectionnez &quot;Utiliser la langue du système&quot; mais AndroidDrive n&apos;est pas disponible dans la langue de votre système, l&apos;anglais sera utilisée. Ce paramètre n&apos;a aucun effet sur le fonctionnement du lecteur en soi. Vous devez redémarrer AndroidDrive pour que ce changement prenne effet.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="55"/>
+        <location filename="settingswindow.cpp" line="58"/>
+        <source>&amp;Language</source>
+        <translation>Lan&amp;gue</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="61"/>
         <source>AndroidDrive doesn&apos;t seem to be available in your system language. &lt;a %1&gt;Click here&lt;/a&gt; if you would like to help translate it.</source>
         <translation>AndroidDrive ne semble pas être disponible dans la langue de votre système. &lt;a %1&gt;Cliquez ici&lt;/a&gt; si cous voulez aider à le traduire.</translation>
     </message>
     <message>
-        <location filename="settingswindow.cpp" line="63"/>
+        <location filename="settingswindow.cpp" line="69"/>
         <source>These settings affect all drives connected with AndroidDrive.</source>
         <translation>Ces paramètres affectent tous les lecteurs connectés avec AndroidDrive.</translation>
     </message>
