201f8f7b45b1b09c13277cad51a63f0d972508aa
GustavLindberg99 <95423695+GustavLindberg99@users.noreply.github.com>
2023-10-05 15:55:21 +0200

    Add files via upload

---
+++ sources/translations/androiddrive_it.ts
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="devicelistwindow.cpp" line="11"/>
+        <location filename="devicelistwindow.cpp" line="82"/>
+        <source>&amp;Connect drive</source>
+        <translation>&amp;Collega unità</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="12"/>
+        <source>Device &amp;settings</source>
+        <translation>&amp;Impostazioni dispositivo</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="14"/>
+        <location filename="devicelistwindow.cpp" line="83"/>
+        <source>Connects a drive containing the internal storage of the selected Android device.</source>
+        <translation>Collega come unità contenente la memoria interna del dispositivo Android selezionato.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="15"/>
+        <source>Allows you to change the settings for this device, for example select a new drive letter or choose whether it should connect automatically.</source>
+        <translation>Consente di modificare le impostazioni per questo dispositivo, ad esempio selezionare una nuova lettera unità o scegliere se connetterlo automaticamente.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="17"/>
+        <source>AndroidDrive - Devices</source>
+        <translation>AndroidDrive - Dispositivi</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="78"/>
+        <source>&amp;Disconnect drive</source>
+        <translation>&amp;Disconnetti unità</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="79"/>
+        <source>Disconnects the drive corresponding to the selected Android device.&lt;br/&gt;&lt;br/&gt;This only disconnects the drive, the Android device itself will remain connected, so you will still be able to access it for example through ADB.</source>
+        <oldsource>Disconnects the drive corresponding to the selected Android device.&lt;br/&gt;&lt;br/&gt;This only disconnects the drive, the Android device itself will remain connected, so you will still be able to access it for example through ADB.&lt;br/&gt;&lt;br/&gt;If you disconnect a drive manually, it won&apos;t be reconnected until you reconnect it manually.</oldsource>
+        <translation>Disconnette l&apos;unità corrispondente al dispositivo Android selezionato.&lt;br/&gt;&lt;br/&gt;Viene disconnessa solo l&apos;unità, il dispositivo Android stesso rimarrà connesso, quindi potrai comunque accedervi, ad esempio tramite ADB.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="91"/>
+        <source>Fatal error: Could not list Android devices.&lt;br/&gt;&lt;br/&gt;ADB exited with code %1.</source>
+        <oldsource>Fatal error: Could not list Android devices. ADB exited with code %1.</oldsource>
+        <translation>Errore irreversibile: impossibile elencare i dispositivi Android.&lt;br/&gt;&lt;br/&gt;ADB è uscito con il codice %1.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="175"/>
+        <source>Could not create a drive with the given drive letter.</source>
+        <translation>Impossibile creare un&apos;unità con la lettera unità specificata.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="182"/>
+        <source>Dokan doesn&apos;t seem to be installed. Would you like to install it now?</source>
+        <translation>Dokan non sembra essere installato. Vuoi installarlo?</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="186"/>
+        <source>Could not start the driver.</source>
+        <translation>Impossibile avviare il driver.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="190"/>
+        <source>Could not assign a drive letter.&lt;br/&gt;&lt;br/&gt;Try changing the drive letter in Device Settings to an available drive letter.</source>
+        <oldsource>Could not assign a drive letter. Try changing the drive letter in Device Settings to an available drive letter.</oldsource>
+        <translation>Impossibile assegnare una lettera unità.&lt;br/&gt;&lt;br/&gt;Prova a modificare in &apos;Impostazioni dispositivo&apos; la lettera unità con una lettera unità disponibile.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="193"/>
+        <source>Dokan version error.</source>
+        <translation>Errore versione Dokan.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="196"/>
+        <source>An unknown error occurred.</source>
+        <translation>Si è verificato un errore sconosciuto.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="199"/>
+        <source>Could not connect device %1: %2</source>
+        <translation>Impossibile connettere il dispositivo %1: %2</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="215"/>
+        <source>ADB timed out.</source>
+        <translation>Timeout ADB.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="218"/>
+        <source>An error occurred when attempting to read from the ADB process.</source>
+        <translation>Si è verificato un errore durante un tentativo di lettura nel processo ADB.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="221"/>
+        <source>An error occurred when attempting to write to the ADB process.</source>
+        <translation>Si è verificato un errore durante il tentativo di scrittura nel processo ADB.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="224"/>
+        <source>ADB failed to start. Either the adb.exe file is missing, or you may have insufficient permissions to invoke the program.</source>
+        <translation>Impossibile avviare ADB. Non è presente il file adb.exe oppure potresti non avere le autorizzazioni sufficienti per eseguire il programma.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="227"/>
+        <source>ADB crashed.</source>
+        <translation>ADB si è crashato.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="230"/>
+        <source>ADB encountered an unknown error.</source>
+        <translation>ADB ha riscontrato un errore sconosciuto.</translation>
+    </message>
+    <message>
+        <location filename="devicelistwindow.cpp" line="233"/>
+        <source>Fatal error: Could not list Android devices: %1</source>
+        <translation>Errore irreversibile: impossibile elencare i dispositivi Android: %1</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="61"/>
+        <source>&amp;Devices</source>
+        <translation>&amp;Dispositivi</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="64"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="67"/>
+        <source>&amp;About AndroidDrive</source>
+        <translation>Info su &amp;AndroidDrive</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="72"/>
+        <source>About AndroidDrive</source>
+        <translation>Info su AndroidDrive</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="73"/>
+        <source>AndroidDrive version %1 by Gustav Lindberg.</source>
+        <translation>AndroidDrive  v. %1 di Gustav Lindberg.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="73"/>
+        <source>Icons made by %3 and %4 from %1 are licensed by %2.</source>
+        <translation>Le icone sono state create da %3 e %4 da %1 e sono concesse in licenza da %2.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="73"/>
+        <source>This program uses %1 and %2.</source>
+        <translation>Questo programma usa %1 e %2.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="77"/>
+        <source>About &amp;Qt</source>
+        <translation>Info su &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="82"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Esci</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="10"/>
+        <source>Global settings</source>
+        <translation>Impostazioni globali</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="11"/>
+        <source>Automatially connect &amp;drive</source>
+        <translation>Connetti automaticamente &amp;unità</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="12"/>
+        <source>Open newly connected drives in &amp;Explorer</source>
+        <translation>Apri unità appena connesse in &amp;Explorer</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="13"/>
+        <source>&amp;Hide files beginning with a dot</source>
+        <translation>&amp;Nascondi file che iniziano con un punto</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="14"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="15"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Annulla</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="16"/>
+        <source>&amp;Apply</source>
+        <translation>A&amp;pplica</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="20"/>
+        <source>AndroidDrive - Settings</source>
+        <translation>AndroidDrive - Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="26"/>
+        <source>Allows you to select a preferred drive letter for the selected Android device.&lt;br/&gt;&lt;br/&gt;If the preferred drive letter is unavailable when this device is connected, it will use the next available drive letter in alphabetical order.&lt;br/&gt;&lt;br/&gt;If you change the drive letter while a drive for this device is connected, you will have to disconnect it and re-connect it again for the changes to take effect.</source>
+        <oldsource>Allows you to select a preferred drive letter for the selected Android device. If the preferred drive letter is unavailable when this device is connected, it will use the next available drive letter in alphabetical order. If you change the drive letter while a drive for this device is connected, you will have to disconnect it and re-connect it again for the changes to take effect.</oldsource>
+        <translation>Consente di selezionare una lettera unità preferita per il dispositivo Android selezionato.&lt;br/&gt;&lt;br/&gt;Se quando il dispositivo viene connesso la lettera unità preferita non è disponibile, verrà usata la lettera unità disponibile successiva in ordine alfabetico.&lt;br/&gt;&lt;br/&gt;Se si modifica la lettera unità mentre è collegata un&apos;unità per questo dispositivo, affinché le modifiche abbiano effetto sarà necessario disconnettere e connettere nuovamente il dispositivo.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="27"/>
+        <source>Drive &amp;letter</source>
+        <translation>&amp;Lettera unità</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="30"/>
+        <source>If this checkbox is checked, the selected device will be automatically connected as a drive whenever you plug it into your computer. Otherwise, you will have to connect it manually by going into Devices &gt; Connect drive.</source>
+        <translation>Se è selezionata questa casella, il dispositivo selezionato verrà automaticamente connesso come unità ogni volta che lo colleghi al computer. 
+Altrimenti, dovrai collegarlo manualmente accedendo a Dispositivi -&gt; Connetti unità.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="33"/>
+        <source>Device settings for %1</source>
+        <translation>Impostazioni dispositivo %1</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="34"/>
+        <source>These settings only affect the selected device.</source>
+        <translation>Queste impostazioni riguardano solo il dispositivo selezionato.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="40"/>
+        <source>If this checkbox is checked, whenever AndroidDrive is finished connecting a drive, it will open that drive in Windows Explorer.</source>
+        <translation>Se è selezionata questa casella di controllo, ogni volta che AndroidDrive termina di connettere un&apos;unità, aprirà quell&apos;unità in Esplora risorse.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="44"/>
+        <source>If this checkbox is checked, files that begin with a dot will be treated as hidden files, and will only be visible in Windows Explorer if Windows Explorer&apos;s &quot;Show hidden files&quot; option is activated.</source>
+        <translation>Se è selezionata questa casella, i file che iniziano con un punto verranno trattati come file nascosti e saranno visibili in Esplora risorse solo se l&apos;opzione &quot;Mostra file nascosti&quot; di Esplora risorse è attivata.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="51"/>
+        <source>Use system language</source>
+        <translation>Usa lingua sistema</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="57"/>
+        <source>Allows you to select which language AndroidDrive&apos;s GUI will be displayed in.&lt;br/&gt;&lt;br/&gt;If you select &quot;Use system language&quot; but AndroidDrive isn&apos;t availiable in your system language, English will be used.&lt;br/&gt;&lt;br/&gt;This setting has no effect on how the actual drive works.&lt;br/&gt;&lt;br/&gt;You must restart AndroidDrive for this change to take effect.</source>
+        <oldsource>Allows you to select which language AndroidDrive&apos;s GUI will be displayed in. If you select &quot;Use system language&quot; but AndroidDrive isn&apos;t availiable in your system language, English will be used. This setting has no effect on how the actual drive works. You must restart AndroidDrive for this change to take effect.</oldsource>
+        <translation>Consente di selezionare la lingua con cui verrà visualizzata la GUI di AndroidDrive.&lt;br/&gt;&lt;br/&gt;Se selezioni &quot;Usa lingua sistema&quot; e AndroidDrive non è disponibile nella lingua del sistema, verrà usata la lingua inglese.&lt;br/&gt;&lt;br/&gt;Questa impostazione non ha alcun effetto sul funzionamento effettivo dell&apos;unità.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="58"/>
+        <source>&amp;Language</source>
+        <translation>&amp;Lingua</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="61"/>
+        <source>AndroidDrive doesn&apos;t seem to be available in your system language. &lt;a %1&gt;Click here&lt;/a&gt; if you would like to help translate it.</source>
+        <translation>AndroidDrive sembra non essere disponibile nella lingua del sistema. Se vuoi aiutarci a tradurlo  &lt;a %1&gt;fai clic qui&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="settingswindow.cpp" line="69"/>
+        <source>These settings affect all drives connected with AndroidDrive.</source>
+        <translation>Queste impostazioni influiscono su tutte le unità connesse con AndroidDrive.</translation>
+    </message>
+    <message>
+        <location filename="updates.cpp" line="26"/>
+        <source>Installing updates...</source>
+        <translation>Installazione aggiornamenti...</translation>
+    </message>
+    <message>
+        <location filename="updates.cpp" line="196"/>
+        <source>An update is available. Do you want to install it now?</source>
+        <translation>È disponibile un aggiornamento. Vuoi installarlo?</translation>
+    </message>
+</context>
+</TS>
