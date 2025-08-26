e5befd3fd2ae42c95b9beaa28b5fd9c2387bdbdc
Gustav Lindberg <95423695+GustavLindberg99@users.noreply.github.com>
2025-08-26 17:57:16 +0200

    Add Chinese translation

--- sources/settingswindow.cpp
+++ sources/settingswindow.cpp
@@ -9,8 +9,8 @@
 
 QSet<SettingsWindow*> SettingsWindow::_instances;
 bool SettingsWindow::systemLanguageAvailable = true;
-const QStringList SettingsWindow::_languageNames{"", "Deutsch", "English", "Français", "Magyar", "Italiano", "日本語", "한국어", "Português Brasileiro", "Svenska"};
-const QStringList SettingsWindow::_languageAbbreviations{"auto", "de", "en", "fr", "hu", "it", "ja", "ko", "pt_BR", "sv"};
+const QStringList SettingsWindow::_languageNames{"", "Deutsch", "English", "Français", "Magyar", "Italiano", "日本語", "한국어", "Português Brasileiro", "Svenska", "中文"};
+const QStringList SettingsWindow::_languageAbbreviations{"auto", "de", "en", "fr", "hu", "it", "ja", "ko", "pt_BR", "sv", "zh_CN"};
 
 SettingsWindow::SettingsWindow(const AndroidDrive *drive):
     _drive(drive)
